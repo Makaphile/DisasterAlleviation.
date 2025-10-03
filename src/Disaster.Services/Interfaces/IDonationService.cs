7e0d71fc201819de4643e81a51acc423e29d8bb1
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:47:18 +0200

    Update IDonationService.cs

--- src/Disaster.Services/Interfaces/IDonationService.cs
+++ src/Disaster.Services/Interfaces/IDonationService.cs
@@ -1,1 +1,12 @@
+using DisasterAlleviation.Models;
+
+namespace DisasterAlleviation.Services
+{
+    public interface IDonationService
+    {
+        IEnumerable<Donation> GetAll();
+        Donation? GetById(int id);
+        void Create(Donation model);
+    }
+}
 
