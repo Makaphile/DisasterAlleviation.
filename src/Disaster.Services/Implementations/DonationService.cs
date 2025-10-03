6437b741ecbc350f1f68ef30f155449dd42564e2
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:47:58 +0200

    Update DonationService.cs

--- src/Disaster.Services/Implementations/DonationService.cs
+++ src/Disaster.Services/Implementations/DonationService.cs
@@ -1,1 +1,23 @@
+using DisasterAlleviation.Data;
+using DisasterAlleviation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DisasterAlleviation.Services
+{
+    public class DonationService : IDonationService
+    {
+        private readonly ApplicationDbContext _db;
+        public DonationService(ApplicationDbContext db) => _db = db;
+
+        public IEnumerable<Donation> GetAll() => _db.Donations.OrderByDescending(d => d.DateDonated).ToList();
+
+        public Donation? GetById(int id) => _db.Donations.Find(id);
+
+        public void Create(Donation model)
+        {
+            _db.Donations.Add(model);
+            _db.SaveChanges();
+        }
+    }
+}
 
