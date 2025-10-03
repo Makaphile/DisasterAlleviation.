e8bbf13585c5d645e4e8027316f67cef1305e6cf
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:50:32 +0200

    Update DonationsController.cs

--- src/Disaster.Web/Controllers/DonationsController.cs
+++ src/Disaster.Web/Controllers/DonationsController.cs
@@ -1,1 +1,30 @@
+using DisasterAlleviation.Models;
+using DisasterAlleviation.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DisasterAlleviation.Controllers
+{
+    public class DonationsController : Controller
+    {
+        private readonly IDonationService _svc;
+        public DonationsController(IDonationService svc) => _svc = svc;
+
+        public IActionResult Index()
+        {
+            var items = _svc.GetAll();
+            return View(items);
+        }
+
+        public IActionResult Create() => View(new Donation { DateDonated = DateTime.UtcNow });
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Donation model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            _svc.Create(model);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
 
