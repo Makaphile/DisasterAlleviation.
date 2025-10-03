bdf79bad0bb591d50a676643be017c685f385b28
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:28:54 +0200

    Update Program.cs

--- src/Disaster.Web/wwwroot/Program.cs
+++ src/Disaster.Web/wwwroot/Program.cs
@@ -1,1 +1,43 @@
+using Disaster.Data;
+using Disaster.Services.Implementations;
+using Disaster.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("DisasterDb"));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IDonationService, DonationService>();
+builder.Services.AddScoped<IIncidentService, IncidentService>();
+builder.Services.AddScoped<IVolunteerService, VolunteerService>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
 
