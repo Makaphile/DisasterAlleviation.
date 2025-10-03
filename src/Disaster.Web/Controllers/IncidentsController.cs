e68c08d9daa3f8f78ba58bea1baadde9d4fb72f3
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:50:57 +0200

    Update IncidentsController.cs

--- src/Disaster.Web/Controllers/IncidentsController.cs
+++ src/Disaster.Web/Controllers/IncidentsController.cs
@@ -1,1 +1,30 @@
+using DisasterAlleviation.Models;
+using DisasterAlleviation.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DisasterAlleviation.Controllers
+{
+    public class IncidentsController : Controller
+    {
+        private readonly IIncidentService _svc;
+        public IncidentsController(IIncidentService svc) => _svc = svc;
+
+        public IActionResult Index()
+        {
+            var items = _svc.GetAll();
+            return View(items);
+        }
+
+        public IActionResult Report() => View(new Incident { DateReported = DateTime.UtcNow });
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Report(Incident model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            _svc.Create(model);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
 
