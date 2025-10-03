734a349525cff85e9070a6e398612ada07e8708d
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:48:20 +0200

    Update IIncidentService.cs

--- src/Disaster.Services/Interfaces/IIncidentService.cs
+++ src/Disaster.Services/Interfaces/IIncidentService.cs
@@ -1,1 +1,12 @@
+using DisasterAlleviation.Models;
+
+namespace DisasterAlleviation.Services
+{
+    public interface IIncidentService
+    {
+        IEnumerable<Incident> GetAll();
+        Incident? GetById(int id);
+        void Create(Incident model);
+    }
+}
 
