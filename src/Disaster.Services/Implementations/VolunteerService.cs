c12df00016e1dd9f43cf09e664bd3175af0e3da3
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:49:25 +0200

    Update VolunteerService.cs

--- src/Disaster.Services/Implementations/VolunteerService.cs
+++ src/Disaster.Services/Implementations/VolunteerService.cs
@@ -1,1 +1,22 @@
+using DisasterAlleviation.Data;
+using DisasterAlleviation.Models;
+
+namespace DisasterAlleviation.Services
+{
+    public class VolunteerService : IVolunteerService
+    {
+        private readonly ApplicationDbContext _db;
+        public VolunteerService(ApplicationDbContext db) => _db = db;
+
+        public IEnumerable<Volunteer> GetAll() => _db.Volunteers.OrderBy(v => v.FullName).ToList();
+
+        public Volunteer? GetById(int id) => _db.Volunteers.Find(id);
+
+        public void Create(Volunteer model)
+        {
+            _db.Volunteers.Add(model);
+            _db.SaveChanges();
+        }
+    }
+}
 
