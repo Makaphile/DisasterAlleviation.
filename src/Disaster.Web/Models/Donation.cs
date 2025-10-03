fa8e507e34976bb56159e07f331b00c3f0db0344
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:46:05 +0200

    Update Donation.cs

--- src/Disaster.Web/Models/Donation.cs
+++ src/Disaster.Web/Models/Donation.cs
@@ -1,1 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DisasterAlleviation.Models
+{
+    public class Donation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string DonorName { get; set; } = string.Empty;
+
+        [Required]
+        public string ResourceType { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateDonated { get; set; } = DateTime.UtcNow;
+    }
+}
 
