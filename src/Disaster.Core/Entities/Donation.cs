fe664b1e0b75f5bb3dfd33ad2c8f3127679bd894
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:30:36 +0200

    Update Donation.cs

--- src/Disaster.Core/Entities/Donation.cs
+++ src/Disaster.Core/Entities/Donation.cs
@@ -1,1 +1,12 @@
+namespace Disaster.Core.Entities
+{
+    public class Donation
+    {
+        public int Id { get; set; }
+        public string DonorName { get; set; }
+        public string ResourceType { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateDonated { get; set; } = DateTime.Now;
+    }
+}
 
