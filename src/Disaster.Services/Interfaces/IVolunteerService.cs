94d9c7add3e26d868f0e8e19261f415ddd4ed767
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:49:07 +0200

    Update IVolunteerService.cs

--- src/Disaster.Services/Interfaces/IVolunteerService.cs
+++ src/Disaster.Services/Interfaces/IVolunteerService.cs
@@ -1,1 +1,12 @@
+using DisasterAlleviation.Models;
+
+namespace DisasterAlleviation.Services
+{
+    public interface IVolunteerService
+    {
+        IEnumerable<Volunteer> GetAll();
+        Volunteer? GetById(int id);
+        void Create(Volunteer model);
+    }
+}
 
