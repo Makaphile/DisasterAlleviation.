1cd4df8c932914ef08eb4d3f864b1213bb74b6bd
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:30:02 +0200

    Update ApplicationDbContext.cs

--- src/Disaster.Data/ApplicationDbContext.cs
+++ src/Disaster.Data/ApplicationDbContext.cs
@@ -1,1 +1,19 @@
+using Disaster.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Disaster.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Donation> Donations { get; set; }
+        public DbSet<Incident> Incidents { get; set; }
+        public DbSet<IncidentImage> IncidentImages { get; set; }
+        public DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<VolunteerTask> VolunteerTasks { get; set; }
+    }
+}
 
