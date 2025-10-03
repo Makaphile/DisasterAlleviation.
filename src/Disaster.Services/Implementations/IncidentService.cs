b4c25dd1f882842ee6577cb41437c6792aeb9681
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:48:46 +0200

    Update IncidentService.cs

--- src/Disaster.Services/Implementations/IncidentService.cs
+++ src/Disaster.Services/Implementations/IncidentService.cs
@@ -1,1 +1,22 @@
+using DisasterAlleviation.Data;
+using DisasterAlleviation.Models;
+
+namespace DisasterAlleviation.Services
+{
+    public class IncidentService : IIncidentService
+    {
+        private readonly ApplicationDbContext _db;
+        public IncidentService(ApplicationDbContext db) => _db = db;
+
+        public IEnumerable<Incident> GetAll() => _db.Incidents.OrderByDescending(i => i.DateReported).ToList();
+
+        public Incident? GetById(int id) => _db.Incidents.Find(id);
+
+        public void Create(Incident model)
+        {
+            _db.Incidents.Add(model);
+            _db.SaveChanges();
+        }
+    }
+}
 
