4a5e307209815f9cd078defb4fc10ef5d8ef18db
Makaphile <130189286+Makaphile@users.noreply.github.com>
2025-10-03 10:51:19 +0200

    Update VolunteersController.cs

--- src/Disaster.Web/Controllers/VolunteersController.cs
+++ src/Disaster.Web/Controllers/VolunteersController.cs
@@ -1,1 +1,30 @@
+using DisasterAlleviation.Models;
+using DisasterAlleviation.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DisasterAlleviation.Controllers
+{
+    public class VolunteersController : Controller
+    {
+        private readonly IVolunteerService _svc;
+        public VolunteersController(IVolunteerService svc) => _svc = svc;
+
+        public IActionResult Index()
+        {
+            var items = _svc.GetAll();
+            return View(items);
+        }
+
+        public IActionResult Register() => View(new Volunteer());
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Volunteer model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            _svc.Create(model);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
 
